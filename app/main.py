dcce6a580ee9ecb8e74ba6c0bcae1492a7e39298
Vidusahan Perera <fc110559@foc.sjp.ac.lk>
2026-04-21 07:57:21 +0530

    feat(expenses): connect routes to main

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from .database import engine, Base
 from . import models
 from .routes import users
+from routes import expenses
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.include_router(expenses.router)
 app.include_router(users.router)
 
 @app.get("/")
